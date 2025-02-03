5defbe414337b303cf04d8c8ab0a05936d276c64
isabelacode <isabelacristina20005@gmail.com>
2025-02-03 20:04:25 -0300

    Adicionando Anuncios

--- src/Components/AdBanner.jsx
+++ src/Components/AdBanner.jsx
@@ -3,15 +3,23 @@ import { useEffect } from 'react';
 export const AdBanner = () => {
    useEffect(() => {
       try {
-         const script = document.createElement('script');
-         script.async = true;
-         script.src =
-            'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+         // Verifica se o script já foi carregado
+         if (
+            !document.querySelector(
+               'script[src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"]'
+            )
+         ) {
+            const script = document.createElement('script');
+            script.async = true;
+            script.src =
+               'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+            document.head.appendChild(script);
+         }
 
-         document.head.appendChild(script);
-
-         // Criando o elemento <ins> do anúncio com as variáveis de ambiente
-         (window.adsbygoogle = window.adsbygoogle || []).push({});
+         // Verifica se o objeto adsbygoogle já existe antes de chamar push
+         if (window.adsbygoogle) {
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+         }
       } catch (e) {
          console.error('Falha ao carregar o anúncio:', e);
       }
